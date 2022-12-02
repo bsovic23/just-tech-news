5750e13c289e7b171d5873a4aaadd6aeb4a1a199
Brit Sovic <bsovic@tulane.edu>
2022-12-02 12:21:21 -0500

    updating user apis

--- models/User.js
+++ models/User.js
@@ -1,45 +1,45 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-// creating user model
-class User extends Model {};
+// create our User model
+class User extends Model {}
 
-// define table columns and configurations
-User.init({
-    id: { 
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+// create fields/columns for User model
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     username: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-            isEmail: true
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            len: [4]
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
     }
-},
-{
-    // sequlize connection
+  },
+  {
     sequelize,
     timestamps: false,
-    freezeTableName: true, 
+    freezeTableName: true,
     underscored: true,
     modelName: 'user'
-}
+  }
 );
 
 module.exports = User;
