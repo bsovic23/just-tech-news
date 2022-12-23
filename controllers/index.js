26568e14c52e4c2b8533033aeff4c68383e40fd0
Brit Sovic <bsovic@tulane.edu>
2022-12-22 19:00:17 -0500

    updating handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const homeroutes = require('./home-routes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeroutes);
 
 router.use((req, res) => {
   res.status(404).end();
